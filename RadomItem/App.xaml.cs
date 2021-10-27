fb2f041c95ea14f9d832ae60a19a7af9c8fe7f89
Color_yr <402067010@qq.com>
2021-10-27 22:15:47 +0800

    up

---
+++ RadomItem/App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace RadomItem
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public const string Version = "1.0.0";
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true;
+                MessageBox.Show("捕获未处理异常:" + e.Exception.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("发生错误" + ex.ToString());
+            }
+
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            StringBuilder sbEx = new StringBuilder();
+            if (e.IsTerminating)
+            {
+                sbEx.Append("发生错误，将关闭\n");
+            }
+            sbEx.Append("捕获未处理异常：");
+            if (e.ExceptionObject is Exception)
+            {
+                sbEx.Append(((Exception)e.ExceptionObject).ToString());
+            }
+            else
+            {
+                sbEx.Append(e.ExceptionObject);
+            }
+            MessageBox.Show(sbEx.ToString());
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            MessageBox.Show("捕获线程内未处理异常：" + e.Exception.ToString());
+            e.SetObserved();
+        }
+    }
+}
---
+++ RadomItem/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadomItem
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        private List<string> list = new List<string>();
+        private bool isGo;
+        private Thread thread;
+        private int now;
+        private bool isRun = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isGo)
+            {
+                isGo = true;
+                Button1.Content = "停止";
+            }
+            else
+            {
+                isGo = false;
+                int a = new Random(Guid.NewGuid().GetHashCode()).Next(0, list.Count - 1);
+                Text1.Content = list[a];
+                Button1.Content = "开始";
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("list.txt"))
+            {
+                MessageBox.Show("没有名单文件list.txt，请新建后再启动", "无抽奖名单");
+                Application.Current.Shutdown();
+                return;
+            }
+            var data  = File.ReadAllText("list.txt");
+            var temp = data.Split('\n');
+            list.AddRange(temp);
+            thread = new Thread(Go);
+            thread.Start();
+        }
+
+        private void Go()
+        {
+            while (isRun)
+            {
+                if (isGo)
+                {
+                    now++;
+                    if (now >= list.Count)
+                    {
+                        now = 0;
+                    }
+                    Dispatcher.Invoke(() =>
+                    {
+                        Text1.Content = list[now];
+                    });
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            isRun = false;
+            isGo = false;
+            thread.Join();
+        }
+    }
+}
